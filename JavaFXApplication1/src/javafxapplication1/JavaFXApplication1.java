a20f9b20ee0fc9fc8db5b78d125a5d5ad3f974a3
unknown <ahmed5666591@gmail.com>
2019-10-25 20:48:06 +0200

    st.

--- JavaFXApplication1/src/javafxapplication1/JavaFXApplication1.java
+++ JavaFXApplication1/src/javafxapplication1/JavaFXApplication1.java
@@ -22,14 +22,7 @@ public class JavaFXApplication1 extends Application {
     @Override
     public void start(Stage primaryStage) {
         Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+        
         
         StackPane root = new StackPane();
         root.getChildren().add(btn);
